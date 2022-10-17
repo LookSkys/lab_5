40e3061c7284e583da7d5c13a912d72a193962d5
Enrique Sanhueza <sanhue.kike@gmail.com>
2022-10-17 16:50:41 -0300

    cambios

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse ("<h1>Informatica USM</h1><hr><nav><lu><li><a href="{% url'home'%}">Home</a></li><li><a href="{% url'carreras'%}">Carreras</a></li><li><a href="{% url'docentes'%}">Docentes</a></li></lu></nav><hr><h3>lorem</h3>")
+    return render(request,'core/home.html')
 
 def carreras(request):
-    return HttpResponse("<h1>Carreras</h1><hr><h2>Menu</h2><hr><h3>lorem<h3>")
+    return 
 
 def docentes(request):
-    return HttpResponse("<h1>Docentes</h1><hr><h2>Menu</h2><hr><h3>lorem<h3>")
+    return 
